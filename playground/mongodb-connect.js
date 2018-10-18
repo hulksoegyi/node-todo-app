e26a2faf8c9de4e225d496ff78ae1a1266c9fda3
Soe Naing Htet <soenaing.htet@gmail.com>
2018-10-18 23:57:04 +0800

    First Commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,40 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+// var user = {name: "SOE NAING HTET", age: 28};
+// var {name} = user;
+// console.log(name);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client)=>{
+    if(err){
+        return console.log(`Unable to connect to MongoDB Server`);
+    }
+    console.log(`Connected to MongoDB Server`);
+    const db = client.db('TodoApp')
+
+    // db.collection('Todos').insertOne({
+    //     text: "Something to do",
+    //     completed: false,
+    // }, (err, result) => {
+    //     if(err){
+    //         return console.log(`Unable to insert to do`, err);
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
+    // db.collection('Users').insertOne({
+    //     name: "SOE NAING HTET",
+    //     age: 28,
+    //     location: "SINGAPORE",
+    // }, (err, result) => {
+    //     if(err){
+    //         return console.log(`Unable to insert`, err);
+    //     }
+    //     console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+    // });
+
+    client.close();
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,45 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client)=>{
+    if(err){
+        return console.log(`Unable to connect to MongoDB Server`);
+    }
+    console.log(`Connected to MongoDB Server`);
+    const db = client.db('TodoApp')
+
+    // db.collection('Todos')
+    //     .find({
+    //         completed: false
+    //     })
+    //     .count()
+    //     .then((count) =>{
+    //         console.log(`Todos count : ${count}`);
+    //     }, (err) => {
+    //         console.log(`Unable to fetch todos. ${err}`);
+    //     });
+
+    // db.collection('Todos')
+    //     .find({
+    //         //_id: "5bc8a73c7f4be73963e3c583",
+    //         _id: new ObjectID("5bc8a27e9400930b70e5778b")
+    //         })
+    //     .toArray()
+    //     .then((doc)=>{
+    //         console.log('Todos');
+    //         console.log(JSON.stringify(doc, undefined, 2));
+    // }, (err) => {
+    //     console.log(`Unable to fetch todos. ${err}`)
+    // });
+
+    db.collection('Users').find({
+        name: { '$ne': 'Mike' },
+    })
+    .toArray().then((docs) => {
+        console.log(`Total Users : ${docs.length}`);
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log(`Unable to fetch users. ${err}`);
+    });
+
+    client.close();
+});
